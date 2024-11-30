dc4fa43c687a5f6659c24c2158aa8fdd642cbf18
gekonody-code <renat.akhmetzh@gmail.com>
2024-11-30 15:28:35 +0300

    feature: changed google api key to yandex

--- precache-manifest.187b3519f422e9989133563500782a72.js
+++ precache-manifest.187b3519f422e9989133563500782a72.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8bf903307c4bf6e25ca3453b76e5f9a7",
+    "revision": "35ee1014e28ad2d345f5bc5419ca33ac",
     "url": "/index.html"
   },
   {
-    "revision": "e2454d5d978337343c46",
+    "revision": "f2f91cee319d7e96d402",
     "url": "/static/css/main.a9089c74.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.1a240082.chunk.js"
   },
   {
-    "revision": "e2454d5d978337343c46",
-    "url": "/static/js/main.f8e4e133.chunk.js"
+    "revision": "f2f91cee319d7e96d402",
+    "url": "/static/js/main.f4793897.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
